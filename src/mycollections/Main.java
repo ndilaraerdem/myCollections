d8342ebaaed29301a632e776d6ce0136702eadf2
Dilara Erdem <ndilaraerdem@gmail.com>
2022-03-09 21:45:07 +0300

    Initial commit

---
+++ src/mycollections/Main.java
@@ -0,0 +1,27 @@
+package mycollections;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        MyArrayList<String> myList = new MyArrayList<String>();
+
+        System.out.println(myList.size());//0
+        myList.add("Ankara");
+        myList.add("İzmir");
+        myList.add("İstanbul");
+        System.out.println(myList.size());//2
+
+        for (Object item : myList.items()) {
+            System.out.println(item);//Ankara İzmir
+        }
+
+
+        //bütün tiplerin ana nesnesi object'tir.
+        //arrayin tipi obje olmalı.
+        //Array fonksiyonlarını kullanmak yazak copy concat vs
+        //length kullanılabilir.
+        //Arraylistler arkada arrayi tutar.
+
+    }
+}
---
+++ src/mycollections/MyArrayList.java
@@ -0,0 +1,35 @@
+package mycollections;
+
+public class MyArrayList<T> {
+    private int element = 0;
+    private Object[] myList;
+    T array;
+
+    public MyArrayList() {
+        myList = new Object[0];
+    }
+
+    public void add(T array) {
+        this.array = array;
+
+        if (element == myList.length) {
+            Object[] temp = myList;
+            myList = new String[myList.length + 1];
+            for (int i = 0; i < temp.length; i++) {
+                myList[i] = temp[i];
+            }
+            myList[element] = this.array;
+            element++;
+
+        }
+    }
+
+    public Object size() {
+        return myList.length;
+    }
+
+    public Object[] items() {
+        return myList;
+    }
+
+}
